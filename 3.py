1baed78b2b77ac24cef5be1c5e65860e438a0778
Roman Abaghayn <84144854+777ARM777@users.noreply.github.com>
2023-06-05 10:48:47 +0400

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,4 +1,5 @@
 import math
 x = int(input("Enter the first number\n"))
 y = int(input("Enter the second number\n"))
-print(abs(x - y) / (x + y))
+if(x != 0 && y != 0):
+  print(abs(x - y) / (x + y))
